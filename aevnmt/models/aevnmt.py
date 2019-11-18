Fixed masking in log_prob of GenerativeLM and TM, copied MADE loss from Victor's branch

aevnmt/models/aevnmt.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Distribution, Independent, Categorical
+from torch.distributions import Distribution, Independent, Categorical, Bernoulli
 
 from aevnmt.components import RNNEncoder, tile_rnn_hidden, tile_rnn_hidden_for_decoder
 from aevnmt.components.nli import DecomposableAttentionEncoder as NLIEncoder
@@ -10,7 +10,7 @@ from aevnmt.dist import NormalLayer, KumaraswamyLayer
 from probabll.distributions import MixtureOfGaussians
 from .inference import get_inference_encoder
 from .inference import InferenceNetwork
-from .generative import GenerativeLM, IndependentLM, GenerativeTM, IBM1TM, IndependentTM
+from .generative import GenerativeLM, IndependentLM, GenerativeTM, IBM1TM, IndependentTM, MADELM
 from itertools import chain
 
 
@@ -20,7 +20,7 @@ class AEVNMT(nn.Module):
     def __init__(self, tgt_vocab_size, emb_size, latent_size, encoder, decoder, language_model,
             pad_idx, dropout, tied_embeddings, prior_family: str, prior_params: list, posterior_family: str,
             inf_encoder_style: str, inf_conditioning: str,feed_z=False,max_pool=False, 
-            bow=False, bow_tl=False, ibm1=False):
+            bow=False, bow_tl=False, ibm1=False, MADE=False):
         super().__init__()
         self.feed_z=feed_z
         self.latent_size = latent_size
@@ -68,6 +68,14 @@ class AEVNMT(nn.Module):
                 src_vocab_size=self.language_model.embedder.num_embeddings, 
                 pad_idx=pad_idx)
             self.aux_lms['bow'] = self.bow_sl_decoder
+        if MADE:
+            self.src_made = MADELM(
+                vocab_size=self.language_model.embedder.num_embeddings, 
+                latent_size=latent_size, 
+                hidden_sizes=[decoder.hidden_size, decoder.hidden_size], 
+                pad_idx=pad_idx,
+                num_masks=10)  # TODO: is this okay?
+            self.aux_lms['made'] = self.src_made
 
         # Auxiliary TMs
         self.aux_tms = dict()
@@ -139,15 +147,13 @@ class AEVNMT(nn.Module):
     def generative_parameters(self):
         # TODO: separate the generative model into a GenerativeModel module
         #  within that module, have two modules, namely, LanguageModel and TranslationModel
-        return chain(self.lm_parameters(), self.tm_parameters(), self.bow_parameters(), self.ibm1_parameters())
+        return chain(self.lm_parameters(), self.tm_parameters(), self.aux_lm_parameters(), self.aux_tm_parameters())
 
-    def bow_parameters(self):
-        sl = iter(()) if 'bow' not in self.aux_lms else self.aux_lms['bow'].parameters()
-        tl = iter(()) if 'bow' not in self.aux_tms else self.aux_tms['bow'].parameters()
-        return chain(sl, tl)
-
-    def ibm1_parameters(self):
-        return iter(()) if 'ibm1' not in self.aux_tms else self.aux_tms['ibm1'].parameters()
+    def aux_lm_parameters(self):
+        return chain(*[model.parameters() for model in self.aux_lms.values()])
+    
+    def aux_tm_parameters(self):
+        return chain(*[model.parameters() for model in self.aux_tms.values()])
 
     def lm_parameters(self):
         return chain(self.language_model.parameters(), self.lm_init_layer.parameters())
@@ -258,10 +264,10 @@ class AEVNMT(nn.Module):
         return torch.cat(tm_logits, dim=1), lm_logits, torch.cat(all_att_weights, dim=1), aux_lm_likelihoods, aux_tm_likelihoods
 
     def log_likelihood_tm(self, comp_name, likelihood: Distribution, y):
-        return self.aux_tms[comp_name].log_likelihood(likelihood, y)
+        return self.aux_tms[comp_name].log_prob(likelihood, y)
     
     def log_likelihood_lm(self, comp_name, likelihood: Distribution, x):
-        return self.aux_lms[comp_name].log_likelihood(likelihood, x)
+        return self.aux_lms[comp_name].log_prob(likelihood, x)
 
     def compute_conditionals(self, x_in, seq_mask_x, seq_len_x, x_out, y_in, y_out, z):
         """

aevnmt/models/generative.py
@@ -10,8 +10,11 @@ These are also known as decoders or generators, here we provide implementations 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Distribution, Categorical
+from torch.distributions import Distribution, Categorical, Bernoulli
 from .nibm1 import NeuralIBM1
+
+from dgm.conditional import MADEConditioner
+from dgm.likelihood import AutoregressiveLikelihood
 
 
 class GenerativeLM(nn.Module):
@@ -20,7 +23,7 @@ class GenerativeLM(nn.Module):
         X_i|z,x_{<i}
     that is, a batch of sequences of torch distribution objects.
 
-    To get the likelihood of an observation, use log_likelihood.
+    To get the likelihood of an observation, use log_prob.
     """
 
     def __init__(self):
@@ -29,7 +32,7 @@ class GenerativeLM(nn.Module):
     def forward(self, x, z) -> Distribution:
         raise NotImplementedError("Implement me!")
 
-    def log_likelihood(self, likelihood: Distribution, x):
+    def log_prob(self, likelihood: Distribution, x):
         raise NotImplementedError("Implement me!")
 
 
@@ -44,16 +47,52 @@ class IndependentLM(GenerativeLM):
         # [B, 1, Vx]
         return Categorical(logits=self.output_layer(z).unsqueeze(1))
     
-    def log_likelihood(self, likelihood: Categorical, x):
+    def log_prob(self, likelihood: Categorical, x):
         # [B, Tx, Vx]
         likelihood = likelihood.expand(x.size())
         # [B, Tx]
         log_p = likelihood.log_prob(x)
         # mask invalid positions
-        log_p = log_p * (x == self.pad_idx).float()
+        log_p = log_p * (x != self.pad_idx).float()
         # [B]
         log_p = log_p.sum(-1)
         return log_p
+
+class MADELM(nn.Module):
+
+    def __init__(self, vocab_size, latent_size, hidden_sizes, pad_idx, num_masks=10):
+        super().__init__()
+        self.pad_idx = pad_idx
+        self.made_conditioner = MADEConditioner(
+            input_size=vocab_size + latent_size, 
+            output_size=vocab_size,  
+            context_size=latent_size,
+            hidden_sizes=hidden_sizes,
+            num_masks=num_masks
+        )
+        self.made = AutoregressiveLikelihood(
+            event_size=vocab_size, 
+            dist_type=Bernoulli,
+            conditioner=self.made_conditioner
+        )
+
+    def forward(self, x, z):
+        bsz = x.size(0)
+        seq_mask_x = x != self.pad_idx
+        made_input = torch.zeros((bsz,self.made.event_size),device=x.device)
+        for i in range(bsz):
+            bow = torch.unique(x[i] * seq_mask_x[i].type_as(x[i]))
+            made_input[i][bow] = 1.0
+        return self.made(z, made_input)
+
+    def log_prob(self, likelihood, x):
+        bsz = x.size(0)
+        made_ref = torch.zeros((bsz,self.made.event_size),device=x.device)
+        for i in range(bsz):
+            bow = torch.unique(x[i])
+            made_ref[i][bow] = 1.0
+        return likelihood.log_prob(made_ref).sum(-1)
+
 
 class GenerativeTM(nn.Module):
     """
@@ -61,7 +100,7 @@ class GenerativeTM(nn.Module):
         Y_j|z,x, y_{<j}
     that is, a batch of sequences of torch distribution objects.
 
-    To get the likelihood of an observation, use log_likelihood.
+    To get the likelihood of an observation, use log_prob.
     """
 
     def __init__(self):
@@ -70,7 +109,7 @@ class GenerativeTM(nn.Module):
     def forward(self, x, seq_mask_x, seq_len_x, y, z) -> Distribution:
         raise NotImplementedError("Implement me!")
 
-    def log_likelihood(self, likelihood: Distribution, y):
+    def log_prob(self, likelihood: Distribution, y):
         raise NotImplementedError("Implement me!")
 
 
@@ -85,13 +124,13 @@ class IndependentTM(GenerativeTM):
         # [B, 1, Vy]
         return Categorical(logits=self.output_layer(z).unsqueeze(1))
     
-    def log_likelihood(self, likelihood: Categorical, y):
+    def log_prob(self, likelihood: Categorical, y):
         # [B, Ty, Vy]
         likelihood = likelihood.expand(y.size())
         # [B, Ty]
         log_p = likelihood.log_prob(y)
         # mask invalid positions
-        log_p = log_p * (y == self.pad_idx).float()
+        log_p = log_p * (y != self.pad_idx).float()
         # [B]
         log_p = log_p.sum(-1)
         return log_p
@@ -116,10 +155,10 @@ class IBM1TM(GenerativeTM):
         g = self.gate(x)
         # [B, Tx, Dz]
         x = self.proj(g * z.unsqueeze(1))
-        # [B, Tx, V]
+        # [B, Ty, V]
         marginals = - self.nibm1(x, seq_mask_x, seq_len_x, y.size(1))
         return Categorical(probs=marginals)
 
-    def log_likelihood(self, likelihood: Categorical, y):
-        return (likelihood.log_prob(y) * (y == self.pad_idx).float()).sum(-1)
+    def log_prob(self, likelihood: Categorical, y):
+        return (likelihood.log_prob(y) * (y != self.pad_idx).float()).sum(-1)
 